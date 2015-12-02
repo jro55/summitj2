986f15897fdcb0f259d6604e55473f11c1fc87f5
Josh Roman <joshproman@yahoo.com>
2015-12-02 10:00:50 -0700

    first commit

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+angular.module('SummitJApp', []);
+
+angular.module('SummitJApp')
+    .controller('summitController', ['$scope', function($scope){
+    	console.log("Hello from summitController")
+    }]);
